b9e0ba03cb36af5f30d85af758e395da0c4e30f3
Hxex <hxex@HX-MacBook-Air.local>
2026-05-24 14:42:46 +0900

    11ty 설정 추가

---
+++ .eleventy.js
@@ -0,0 +1,24 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("images");
+
+  eleventyConfig.addCollection("gallery", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("_posts/gallery/*.md");
+  });
+
+  eleventyConfig.addCollection("notice", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("_posts/notice/*.md");
+  });
+
+  eleventyConfig.addCollection("resources", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("_posts/resources/*.md");
+  });
+
+  return {
+    dir: {
+      input: ".",
+      output: "_site",
+      includes: "_includes"
+    }
+  };
+};
